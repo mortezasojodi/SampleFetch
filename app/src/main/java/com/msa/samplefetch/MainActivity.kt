5b7d9d6de8f4ffceff12c50cc1e2d192ceff5b9d
morteza <mortezasojodi70@gmail.com>
2024-09-02 17:25:36 +0330

    remove unused

--- app/src/main/java/com/msa/samplefetch/MainActivity.kt
+++ app/src/main/java/com/msa/samplefetch/MainActivity.kt
@@ -18,8 +18,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            // A surface container using the 'background' color from the theme
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
@@ -28,20 +26,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-
-@Composable
-fun GreetingPreview() {
-
-    Greeting("Android")
-
 }
